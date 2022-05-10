ddcd3455de2692bea83bb95fd6c9cb5e444236f9
Jeronimo Barea Lucas <jeronimobarea@Jeronimos-MacBook-Pro.local>
2022-05-10 18:01:13 +0200

    refactor(common): test utils

--- common/utils_test.go
+++ common/utils_test.go
@@ -8,19 +8,18 @@ import (
 	"github.com/tj/assert"
 )
 
-/*
- * GetAddressFromPrivateKey
- */
-func Test_GetAddressFromPrivateKey_ValidKey(t *testing.T) {
-	validKey, err := crypto.HexToECDSA("09e910621c2e988e9f7f6ffcd7024f54ec1461fa6e86a4b545e9e1fe21c28866")
-	assert.NoError(t, err)
-	address, err := GetAddressFromPrivateKey(validKey)
-	assert.NoError(t, err)
-	assert.Equal(t, common.HexToAddress("0x00B54E93EE2EBA3086A55F4249873E291D1AB06C"), address)
-}
+func Test_GetAddressFromPrivateKey(t *testing.T) {
+	t.Run("valid key", func(t *testing.T) {
+		validKey, err := crypto.HexToECDSA("09e910621c2e988e9f7f6ffcd7024f54ec1461fa6e86a4b545e9e1fe21c28866")
+		assert.NoError(t, err)
+		address, err := GetAddressFromPrivateKey(validKey)
+		assert.NoError(t, err)
+		assert.Equal(t, common.HexToAddress("0x00B54E93EE2EBA3086A55F4249873E291D1AB06C"), address)
+	})
 
-func Test_GetAddressFromPrivateKey_NilKey(t *testing.T) {
-	address, err := GetAddressFromPrivateKey(nil)
-	assert.Error(t, err)
-	assert.Equal(t, common.Address{}, address)
+	t.Run("nil key", func(t *testing.T) {
+		address, err := GetAddressFromPrivateKey(nil)
+		assert.Error(t, err)
+		assert.Equal(t, common.Address{}, address)
+	})
 }
